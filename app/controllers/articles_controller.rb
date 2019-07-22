3a8e8b1634400f04b88868276a9d9cde7c467246
wc_nilesh <wc.nilesh@gmail.com>
2019-07-22 16:18:08 +0530

    Deployed Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
 
   def new
     @article = Article.new
@@ -15,10 +20,36 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
+  end
+
+  def destroy
+
+    @article.destroy
+    flash[:notice] = "Article Deleted Successfully"
+    redirect_to articles_path
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @article.update(article_params)
+      flash[:notice] = "Article Updated Successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end

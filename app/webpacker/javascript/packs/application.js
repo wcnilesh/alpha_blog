4b32bfa6051a95b6b6496824109884dcf94b7e06
wc_nilesh <wc.nilesh@gmail.com>
2019-07-25 12:05:33 +0530

    Associations

--- app/webpacker/javascript/packs/application.js
+++ app/webpacker/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
-
 import 'bootstrap'
 import '../stylesheets/application.scss'
 
